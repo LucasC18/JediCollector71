3785a85c27f6d64743c63219eaeb48b8429db23c
LucasC18 <lucascohanedu@gmail.com>
2025-12-24 12:48:51 -0300

    fix missing context providers in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
 import "./index.css";
 
+// ⬇️ IMPORTÁ TUS PROVIDERS
+import { AuthProvider } from "@/context/AuthContext";
+import { CartProvider } from "@/context/CartContext";
+import { ProductProvider } from "@/context/ProductContext";
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/mikeco-frontend">
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <ProductProvider>
+        <CartProvider>
+          <BrowserRouter basename="/mikeco-frontend">
+            <App />
+          </BrowserRouter>
+        </CartProvider>
+      </ProductProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
